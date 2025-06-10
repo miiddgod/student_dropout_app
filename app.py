59b6fcdf13f8a1dee5ccaaf3bd7d0ed7b453e0f6
miiddgod <hafizhbachtiyar4@gmail.com>
2025-06-10 20:09:34 +0700

    update

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import joblib
-from PIL import Image
 
 # Page configuration
 st.set_page_config(page_title="Student Dropout Prediction", layout="wide")
